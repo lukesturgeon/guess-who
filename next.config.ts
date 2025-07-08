6a891326ee0951c8e74f7c5e3bd6d5c1cc810979
Luke Sturgeon <hello@lukesturgeon.co.uk>
2025-07-08 16:30:36 +0100

    fixed depreciated image loading

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["oaidalleapiprodscus.blob.core.windows.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+      },
+    ],
   },
 };
 
