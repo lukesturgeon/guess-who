679737faf9e1ace88bdb8c1467b4e569c3f2584f
Luke Sturgeon <hello@lukesturgeon.co.uk>
2025-07-08 16:31:01 +0100

    moved code to clean up files

--- components/main.tsx
+++ components/main.tsx
@@ -3,69 +3,7 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
-type AnimalCard = {
-    image: string;
-    description: string;
-};
-
-
-// List of 20 animals for the guessing game
-const ANIMAL_NAMES = [
-    "Elephant",
-    "Giraffe",
-    "Lion",
-    "Tiger",
-    "Zebra",
-    "Kangaroo",
-    "Panda",
-    "Penguin",
-    "Monkey",
-    "Rabbit",
-    "Horse",
-    "Bear",
-    "Fox",
-    "Wolf",
-    "Deer",
-    "Owl",
-    "Dolphin",
-    "Shark",
-    "Crocodile",
-    "Flamingo"
-];
-
-// List of 20 fun, imaginative scenarios for animals
-const ANIMAL_SCENARIOS = [
-    "jumping out of an airplane",
-    "landing on the moon",
-    "riding a skateboard",
-    "eating a giant ice cream",
-    "playing video games",
-    "surfing a huge wave",
-    "having a tea party",
-    "dancing in a disco",
-    "painting a masterpiece",
-    "building a sandcastle",
-    "playing soccer",
-    "flying a kite",
-    "reading a book in a library",
-    "going on a treasure hunt",
-    "baking a cake",
-    "singing on stage",
-    "exploring a jungle",
-    "riding a roller coaster",
-    "having a snowball fight",
-    "taking selfies with friends"
-];
-
-// Generates an AnimalCard with a random animal name
-function generateAnimalCard(): AnimalCard {
-    const animal = ANIMAL_NAMES[Math.floor(Math.random() * ANIMAL_NAMES.length)];
-    const scenario = ANIMAL_SCENARIOS[Math.floor(Math.random() * ANIMAL_SCENARIOS.length)];
-    return {
-        image: `/shark.jpg`,
-        description: `${animal} ${scenario}`
-    };
-}
+import { AnimalCard, generateAnimalCard } from "../utils/animalCard";
 
 
 
@@ -94,7 +32,7 @@ export default function Main() {
             if (!data.image) throw new Error('No image returned');
             // Only update the image property, keep the rest of the card
             setSelectedCard(card => card ? { ...card, image: data.image } : null);
-        } catch (err: unknown) {    
+        } catch (err: unknown) {
             if (err instanceof Error) {
                 setError(err.message);
             } else {
